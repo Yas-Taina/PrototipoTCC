d10c5d20640d84acce89e1c92935c9ac27acca7e
MateusDV <mateusdevitata@gmail.com>
2025-10-09 20:02:02 -0300

    feat: adiciona validação no formulário

--- prototipo-tcc-front/src/app/pages/form/form.component.ts
+++ prototipo-tcc-front/src/app/pages/form/form.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { DataPointsService } from '../../services/data-points.service';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -13,14 +13,21 @@ export class FormComponent {
   readonly builder = inject(FormBuilder);
 
   form = this.builder.group({
-    type: ['x'],
-    date: [new Date()],
-    value: [0]
+    type: ['x', [Validators.required]],
+    date: [null, [Validators.required]],
+    value: [0, [Validators.required, Validators.min(1)]]
   });
 
   constructor() { }
 
   onSubmit(data: any) {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+
     this.dataPointsService.Save(data);
+
+    alert("Dados registrados com sucesso");
   }
 }
