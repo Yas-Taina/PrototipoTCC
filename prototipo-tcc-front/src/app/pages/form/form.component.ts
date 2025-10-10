35d86bc29cc0996c78f0c15a2eb5a21b3eb93275
MateusDV <mateusdevitata@gmail.com>
2025-10-10 20:09:26 -0300

    fix: adicionar e melhorar certas validações e pontos visuais

--- prototipo-tcc-front/src/app/pages/form/form.component.ts
+++ prototipo-tcc-front/src/app/pages/form/form.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { DataPointsService } from '../../services/data-points.service';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-form',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
@@ -15,7 +16,7 @@ export class FormComponent {
   form = this.builder.group({
     type: ['x', [Validators.required]],
     date: [null, [Validators.required]],
-    value: [0, [Validators.required, Validators.min(1)]]
+    value: [0, [Validators.required, Validators.min(0)]]
   });
 
   constructor() { }
